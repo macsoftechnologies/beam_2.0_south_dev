e485a6eaad83ea8b6ca3d2c6d018183e41d57dc3
macsoftechnologies <macsoftechnologies@gmail.com>
2026-08-18 19:15:23 +0530

    feat: initialize NestJS backend application with global configuration, validation, and static file serving

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { setupSwagger } from './swagger/swagger.setup';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') ?? 3001;
+  const port = configService.get<number>('PORT') ?? 5200;
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true, }),);
   app.enableCors();
